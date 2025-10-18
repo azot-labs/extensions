5b67f9df4e0ee45696d3421521940f6aca207a90
Vitaly Gashkov <vitalygashkov@vk.com>
2025-10-18 18:06:42 +0500

    fix: update favicon url

--- docs/.vitepress/config/theme.ts
+++ docs/.vitepress/config/theme.ts
@@ -1,5 +1,6 @@
 import type { DefaultTheme, HeadConfig, LocaleConfig } from 'vitepress';
 import { createTranslate } from '../i18n/utils';
+import { FAVICON, DOMAIN, GITHUB, NPM } from './shared';
 
 export function getLocaleConfig(lang: string) {
   const t = createTranslate(lang);
@@ -9,19 +10,16 @@ export function getLocaleConfig(lang: string) {
   const description = t('Start building your perfect extensions with the Azot API');
   const titleTemplate = `:title | ${description}`;
 
-  const docsLink = `https://developers.azot.so/`;
-  const ogImage = `https://azot.so/favicon.ico`;
-
   const head: HeadConfig[] = [
-    ['link', { rel: 'icon', type: 'image/svg+xml', href: 'https://azot.so/favicon.ico' }],
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: FAVICON }],
     ['meta', { name: 'theme-color', content: '#ff7e17' }],
     ['meta', { property: 'og:title', content: title }],
     ['meta', { property: 'og:description', content: description }],
-    ['meta', { property: 'og:image', content: ogImage }],
+    ['meta', { property: 'og:image', content: FAVICON }],
     ['meta', { property: 'og:type', content: 'website' }],
-    ['meta', { property: 'og:url', content: docsLink }],
+    ['meta', { property: 'og:url', content: DOMAIN }],
     ['meta', { property: 'twitter:card', content: 'summary_large_image' }],
-    ['meta', { property: 'twitter:image', content: ogImage }],
+    ['meta', { property: 'twitter:image', content: FAVICON }],
   ];
 
   const nav: DefaultTheme.NavItem[] = [{ text: t('Home'), link: `${urlPrefix}/` }];
@@ -42,17 +40,17 @@ export function getLocaleConfig(lang: string) {
   ];
 
   const themeConfig: DefaultTheme.Config = {
-    logo: { src: 'https://azot.so/favicon.ico', width: 24, height: 24 },
+    logo: { src: FAVICON, width: 24, height: 24 },
     nav,
     sidebar,
     outline: 'deep',
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/azot-labs/extensions' },
-      { icon: 'npm', link: 'https://www.npmjs.com/package/@streamyx/api' },
+      { icon: 'github', link: GITHUB },
+      { icon: 'npm', link: NPM },
     ],
     footer: {
       message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2025-present Vitaly Gashkov & Contributors',
+      copyright: 'Copyright © 2025-present Azot',
     },
   };
 
